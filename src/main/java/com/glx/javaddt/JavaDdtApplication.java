c047b0ea8c39636b90637f0a1fc72b2dad6734b3
Bramanta Nararya Nurul Haq <bramanta.n.n.haq@GDPL-LTP-889.local>
2022-11-28 09:22:55 +0700

    init spring project

---
+++ src/main/java/com/glx/javaddt/JavaDdtApplication.java
@@ -0,0 +1,13 @@
+package com.glx.javaddt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDdtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDdtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glx/javaddt/JavaDdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glx.javaddt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDdtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
